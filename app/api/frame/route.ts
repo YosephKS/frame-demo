26cbb1258c8b07b03df9fadef683c3783787e9a9
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@gmail.com>
2024-02-08 18:41:56 +0800

    integrate airstack data to resolve Fnames

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -1,25 +1,63 @@
 import { FrameRequest } from '@coinbase/onchainkit';
 import { NextRequest, NextResponse } from 'next/server';
 import { SUCCESS_CLAIM_IMAGE_URL } from '../../lib/constants';
+import { init, fetchQuery } from '@airstack/node';
+
+init(process.env.AIRSTACK_API_KEY ?? '');
+
+const query = /* GraphQL */ `
+  query MyQuery($upFid: String!, $downFid: String!) {
+    up: Socials(input: { filter: { userId: { _eq: $upFid } }, blockchain: ethereum }) {
+      Social {
+        profileName
+      }
+    }
+    down: Socials(input: { filter: { userId: { _eq: $downFid } }, blockchain: ethereum }) {
+      Social {
+        profileName
+      }
+    }
+  }
+`;
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   const body: FrameRequest = await req.json();
   const { fid } = body?.untrustedData ?? {};
-  return new NextResponse(`
+  const upFid = (fid + 1).toString();
+  const downFid = (fid - 1).toString();
+  const { data, error } = await fetchQuery(query, { upFid, downFid });
+  if (!error) {
+    const up = data?.up?.Social?.[0]?.profileName;
+    const down = data?.down?.Social?.[0]?.profileName;
+    return new NextResponse(`
         <!DOCTYPE html>
           <html>
             <head>
               <meta property="fc:frame" content="vNext" />
               <meta property="fc:frame:image" content="${SUCCESS_CLAIM_IMAGE_URL}" />
-              <meta name="fc:frame:button:1" content="${fid - 1}" />
+              <meta name="fc:frame:button:1" content="@${down}" />
               <meta name="fc:frame:button:1:action" content="link" />
-              <meta name="fc:frame:button:1:target" content="https://explorer.airstack.xyz/token-balances?address=fc_fid:${fid - 1}&rawInput=%23%E2%8E%B1fc_fid:${fid - 1}%E2%8E%B1%28fc_fid:${fid - 1}++ethereum+null%29&inputType=ADDRESS" />
-              <meta name="fc:frame:button:2" content="${fid + 1}" />
+              <meta name="fc:frame:button:1:target" content="https://explorer.airstack.xyz/token-balances?address=fc_fid:${downFid}&rawInput=%23%E2%8E%B1fc_fid:${downFid}%E2%8E%B1%28fc_fid:${fid - 1}++ethereum+null%29&inputType=ADDRESS" />
+              <meta name="fc:frame:button:2" content="@${up}" />
               <meta name="fc:frame:button:2:action" content="link" />
-              <meta name="fc:frame:button:2:target" content="https://explorer.airstack.xyz/token-balances?address=fc_fid:${fid + 1}&rawInput=%23%E2%8E%B1fc_fid:${fid + 1}%E2%8E%B1%28fc_fid:${fid + 1}++ethereum+null%29&inputType=ADDRESS" />
+              <meta name="fc:frame:button:2:target" content="https://explorer.airstack.xyz/token-balances?address=fc_fid:${upFid}&rawInput=%23%E2%8E%B1fc_fid:${upFid}%E2%8E%B1%28fc_fid:${fid + 1}++ethereum+null%29&inputType=ADDRESS" />
           </head>
         </html>
     `);
+  } else {
+    return new NextResponse(`
+        <!DOCTYPE html>
+          <html>
+            <head>
+              <meta property="fc:frame" content="vNext" />
+              <meta property="fc:frame:image" content="https://gateway.ipfs.io/ipfs/QmSho42fWi25oBWp5MrrFH9zUGFvnspxVHvcymNJoe3DEZ/output.png" />
+              <meta name="fc:frame:button:1" content="Try again?" />
+              <meta name="fc:frame:button:1:action" content="post" />
+              <meta name="fc:frame:button:1:target" content="https://frame-demo-seven.vercel.app/api/frame" />
+          </head>
+        </html>
+    `);
+  }
 }
 
 export async function POST(req: NextRequest): Promise<Response> {
